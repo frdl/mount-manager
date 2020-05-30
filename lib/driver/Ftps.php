5ca4e3410ff37823bb4c38006cf793304f698e2c
Till Wehowski <github@webfan.de>
2020-05-31 00:34:24 +0200

    Create Ftps.php

---
+++ lib/driver/Ftps.php
@@ -0,0 +1,29 @@
+<?php
+namespace frdl\mount\driver;
+
+use frdl\mount\Manager;
+use frdl\mount\Driver;
+use frdl\mount\Exception;
+
+use frdl\mount\driver\Ftp;
+
+/**
+ * MagicMounter, by Marvin Janssen (http://marvinjanssen.me), released in 2017.
+ *
+ * The FTPS magic driver provides a transparent FTPS transport. It extends \MagicMounter\driver\Ftp
+ */
+class Ftps extends Ftp
+	{
+	public function __construct(array $options)
+		{
+		if (!extension_loaded('openssl') || !function_exists('ftp_ssl_connect'))
+			throw new Exception('OpenSSL extension not available',103);
+      
+		  parent::__construct($options);
+		}
+
+	protected function ftp_connect($host,$port,$timeout)
+		{
+		  return ftp_ssl_connect($host,$port,$timeout);
+		}
+	}
