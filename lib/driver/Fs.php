8bab40337110214491ee16a3377298d7034ebe48
Till Wehowski <github@webfan.de>
2020-05-31 09:39:35 +0200

    Update Fs.php

--- lib/driver/Fs.php
+++ lib/driver/Fs.php
@@ -6,6 +6,8 @@ use frdl\mount\Manager;
 use frdl\mount\Exception;
 use frdl\mount\Driver;
 
+use frdl\ContextContainer;
+
 /**
  * MagicMounter, by Marvin Janssen (http://marvinjanssen.me), released in 2017.
  * 
