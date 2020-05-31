f8bd5643ff2f7b7cbc8a40efb32cdab4bba8595b
Till Wehowski <github@webfan.de>
2020-05-31 06:31:56 +0200

    Create StreamMapping.php

---
+++ lib/driver/Mapping/StreamMapping.php
@@ -0,0 +1,10 @@
+<?php
+namespace frdl\mount\driver\Mapping;
+
+final class StreamMapping
+{
+   public function __construct(string $protocol, bool $writable, array $hostsLocationMapping){
+	   
+   }
+	
+}
