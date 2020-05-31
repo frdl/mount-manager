f1ed28126b45d42b232adc3f51a80469a6835853
Till Wehowski <github@webfan.de>
2020-05-31 06:53:36 +0200

    Update StreamMapping.php

--- lib/driver/Mapping/StreamMapping.php
+++ lib/driver/Mapping/StreamMapping.php
@@ -1,10 +1,25 @@
 <?php
 namespace frdl\mount\driver\Mapping;
 
-final class StreamMapping
+use frdl\mount\driver\Mapping\DNS;
+
+final class StreamMapping extends \IteratorIterator
 {
-   public function __construct(string $protocol, bool $writable, array $hostsLocationMapping){
-	   
+   protected $protocol;
+   protected $writable;	
+   protected $map;	
+	
+   public function __construct(string $protocol, bool $writable, DNS $mountDNS/*\Generator $map = null*/){
+	   $this->protocol = $protocol;
+	   $this->writable = $writable;
+	   $this->map = $this->createMapping($mountDNS);
    }
+	
+	
+  public function createMapping(DomainMount... $entries){
+	    while ($mount = array_shift($entries)) {  
+	        yield $mount; 
+	    }
+  }
 	
 }
