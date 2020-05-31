5c197e0bb92d2d026201365a5c5efe3612f666a1
Till Wehowski <github@webfan.de>
2020-05-31 09:42:46 +0200

    Update Ftps.php

--- lib/driver/Ftps.php
+++ lib/driver/Ftps.php
@@ -13,13 +13,27 @@ use frdl\mount\driver\Ftp;
  * The FTPS magic driver provides a transparent FTPS transport. It extends \MagicMounter\driver\Ftp
  */
 class Ftps extends Ftp
-	{
-	public function __construct(array $options)
+{
+	protected $options;
+	
+	
+	
+	public function __construct( $options)
 		{
+		
+				
+		$this->options=(!is_object($options) || true!==$options instanceof ContextContainer)
+			? ContextContainer::create($options, '${', '}')
+			: $options	
+			;
+		
+		
+		
+		
 		if (!extension_loaded('openssl') || !function_exists('ftp_ssl_connect'))
 			throw new Exception('OpenSSL extension not available',103);
       
-		  parent::__construct($options);
+		  parent::__construct($this->options);
 		}
 
 	protected function ftp_connect($host,$port,$timeout)
