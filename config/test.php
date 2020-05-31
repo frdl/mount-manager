a855c148a6825a2bb0123204e026156e4896393d
Till Wehowski <github@webfan.de>
2020-05-31 21:12:59 +0200

    Create test.php

---
+++ config/test.php
@@ -0,0 +1,48 @@
+<?php
+
+use Psr\Container\ContainerInterface;
+
+
+use function DI\factory;
+use function DI\decorate;
+use function DI\get;
+use function DI\add;
+
+use frdl\mount\Manager;
+
+use bovigo\vfs\vfsStream;
+use bovigo\vfs\vfsStreamDirectory;
+use bovigo\vfs\vfsStreamWrapper;
+
+
+return [
+	Manager::class => function(ContainerInterface $c){
+    $Manager = Manager::getInstance('web+fan', 'test');
+		return $Manager;
+	},	
+	
+	
+'config.managers.mounting' => add([		
+         [
+              'name' => 'vhost.test',
+              'scheme' =>  'webf+vfs',
+              'type' => 'virtual',
+              ''options' => [
+                   'root' => '~',
+                    'fs.virtual.structure' => [
+                    
+                    ],
+                    'directory' => null,
+                    
+                    'target' => new vfsStreamWrapper
+              ],
+         ]
+	]),
+
+];
+
+
+		  
+		  
+	 
+      
