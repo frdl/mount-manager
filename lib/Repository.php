bc87e4c908db9f01caf1f6b5f1e0929647360e89
Till Wehowski <github@webfan.de>
2020-05-31 15:47:23 +0200

    Update Repository.php

--- lib/Repository.php
+++ lib/Repository.php
@@ -4,15 +4,68 @@ namespace frdl\mount;
 
 use frdl\mount\Driver;
 
-class Repository {
+class Repository
+{
 
-    public function __construct(Driver $driver)
+    const METHOD_MAP = [
+        'singleton',
+        'quote',
+        'stream_open',
+        'stream_eof',
+        'stream_stat',
+        'stream_seek',
+        'stream_tell',
+        'stream_truncate',
+        'stream_write',
+        'stream_set_option',
+        'stream_lock',
+        'stream_flush',
+        'stream_cast',
+        'stream_close',
+        'unlink',
+        'url_stat',
+        'stream_metadata',
+        'mkdir',
+        'rmdir',
+        'rename',
+        'dir_opendir',
+        'dir_closedir',
+        'dir_readdir',
+        'dir_rewinddir',
+                      
+    ];
+    const METHOD_MAP_STATIC = [
+        'getOptions'
+    ];
+    
+    protected $driver;
+    protected static $classNames = [];
+    
+    public function __construct(Driver $driver,string $type = null)
     {
         $this->driver = $driver;
+        if(\is_string($type) && !isset(self::$classNames[$type])){
+            self::$classNames[$type] = get_class($this->driver);
+        }
     }
+    
+    public static function call($method, $arguments, string $type = null){
+       
+        if(\is_string($type) ){
+              $class = self::$classNames[$type];     
+        }else{  
+               $class = Driver::class;          
+         }
+        
+           return call_user_func_array($class.'::'.$method, $arguments); 
+    }
+    
+    public function __call($method, $arguments){
+        if(\in_array($method, self::METHOD_MAP)){
+           return call_user_func_array([$this->driver, $method], $arguments);   
+        }
+    }
+    
+    
 
-    public static function getOptions() :array
-    {
-        return $this->driver::getOptions();
-    }
 }
