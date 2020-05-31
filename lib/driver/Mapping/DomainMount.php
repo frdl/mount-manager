9015eebcc89c2efabe7ec96113d02879fc2d32f8
Till Wehowski <github@webfan.de>
2020-05-31 07:39:10 +0200

    Update DomainMount.php

--- lib/driver/Mapping/DomainMount.php
+++ lib/driver/Mapping/DomainMount.php
@@ -1,18 +1,31 @@
 <?php
 namespace frdl\mount\driver\Mapping;
 
-/*
-final class DomainMount
+
+use steevanb\PhpTypedArray\{
+    Exception\InvalidTypeException,
+    ScalarArray\StringArray,
+    ObjectArray\CodePointStringArray
+};
+
+
+
+final class DomainMount extends CodePointStringArray
 {
 	   
-   public function __construct($host, $location){
-	    parent::__construct(new \ArrayIterator($entries));
-   }
-	
-  public function current() : DomainMount
-  {
-    return parent::current();
-  }
+    public function __construct(string $host, string $location)
+    {
+        parent::__construct([$host, $location], CodePointString::class);
+	if(2!==$this->count()){
+	  throw new \Exception('Invalid array count (key,value=2 required) in '.__METHOID__);	
+	}
+    }
   
+    public function toArray(): array
+    {
+        return [$this->values[0] => $this->values[1]];
+    }
+   	
+	
 }
-*/
+
