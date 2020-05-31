6132959ed47b6b47cb2176f49cc0ec1b4b03cfd3
Till Wehowski <github@webfan.de>
2020-05-31 07:13:34 +0200

    Create DomainMount.php

---
+++ lib/driver/Mapping/DomainMount.php
@@ -0,0 +1,18 @@
+<?php
+namespace frdl\mount\driver\Mapping;
+
+/*
+final class DomainMount
+{
+	   
+   public function __construct($host, $location){
+	    parent::__construct(new \ArrayIterator($entries));
+   }
+	
+  public function current() : DomainMount
+  {
+    return parent::current();
+  }
+  
+}
+*/
