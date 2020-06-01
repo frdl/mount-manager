f758dd35db3ceb35208e67020c622dcc54eb6c89
Till Wehowski <github@webfan.de>
2020-06-01 13:23:34 +0200

    Create extension.php

---
+++ level-2/configurations/extension.php
@@ -0,0 +1,64 @@
+<?php
+
+use Psr\Container\ContainerInterface;
+
+
+use function DI\factory;
+use function DI\decorate;
+use function DI\get;
+use function DI\add;
+
+use frdl\mount\Manager;
+
+use bovigo\vfs\vfsStream;
+use bovigo\vfs\vfsStreamDirectory;
+use bovigo\vfs\vfsStreamWrapper;
+
+
+return [
+ 'extensions' => add([
+       'mountmanager' => [
+             'key' => 'mounting',
+             'name' => 'MountManager',
+             'manager' => Manager::class,
+       ],
+ ]),
+ 'services' => add([
+ 
+ ]),
+ 'managers' => add([
+        'mounting' => [
+             'class' => Manager::class,
+        ],
+ 
+ ]),
+ 'drivers' => [
+ 
+ ], 
+ 'mounts' => [
+ 
+ ],
+
+ 'protocols' => [
+ 
+ ],
+
+
+
+ 'domains' => [
+ 
+ ],
+
+
+
+ 'stages' => [
+ 
+ ],
+
+
+];
+
+
+		  
+		  
+	 
